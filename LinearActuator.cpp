cc370c13635cd0caab5b567268a4d2affe020ec6
uamp <uamp@users.noreply.github.com>
2017-05-20 00:50:05 +0100

    Initial commit

--- LinearActuator.cpp
+++ LinearActuator.cpp
@@ -7,6 +7,7 @@ LinearActuator::LinearActuator(uint8_t _pin_A, uint8_t _pin_B, uint8_t _pin_enab
 	pinMode(pin_B,OUTPUT);
 	pinMode(pin_enable,OUTPUT);
 	pinMode(pin_current_sense,INPUT);
+	motor_delay=500;
 }
 
 void LinearActuator::motorControl(bool motor_on, bool direction=true){
@@ -15,6 +16,70 @@ void LinearActuator::motorControl(bool motor_on, bool direction=true){
   	digitalWrite(pin_B,!direction);
 }
 
+uint16_t LinearActuator::readCurrent(){  
+	return analogRead(pin_current_sense);
+}
+
+void LinearActuator::setPosition(uint8_t demanded_position){
+	uint32_t time_start,travel_time;
+	if(demanded_position==current_position) {
+		return;
+	}
+	if(demanded_position>current_position){
+		direction=true;
+		travel_time=(demanded_position-current_position)*throw_time/255;
+	}
+	if(demanded_position<current_position){
+		direction=true;
+		travel_time=(current_position-demanded_position)*throw_time/255;
+	}
+
+	time_start=millis();
+	motorControl(true,direction); //motoring
+	delay(motor_delay);
+	do{
+	}while(readCurrent < stall_current && (millis()-time_start)<(travel_time));
+	current_position=demanded_position; //crude, refined below
+	if(readCurrent>stall_current){ //update position if end-stop reached
+		if(direction==true) current_position=255;
+		if(direction==false) current_position=0;
+	}
+	motorControl(false);//stop motor
+	
+}
+
+uint8_t LinearActuator::getPosition(){
+	return current_position;
+}
+
+void LinearActuator::calibrate(){
+
+	uint32_t time_start;
+	time_start=millis();
+	
+	motorControl(true,true); //motoring out
+	delay(motor_delay);
+	do{
+	}while(readCurrent < stall_current && (millis()-time_start)<(2*throw_time));
+
+	time_start=millis();
+	otorControl(true,false); //motoring back in
+	delay(motor_delay);
+	do{
+	}while(readCurrent < stall_current && (millis()-time_start)<(2*throw_time));
+	motorControl(false); //stop motor
+	throw_time=(millis()-time_start);
+	current_position=0;
+	
+}
+
+void LinearActuator::setStallCurrent(uint16_t _stall_current){
+	if(_stall_current<1024) stall_current=_stall_current;
+}
+
+void LinearActuator::setThrowTime(uint32_t _throw_time);
+	throw_time=_throw_time;
+}
 
 hbridge::~hbridge(){
 }
--- LinearActuator.h
+++ LinearActuator.h
@@ -9,13 +9,26 @@ private:
 	uint8_t pin_B;
 	uint8_t pin_enable;
 	uint8_t pin_current_sense;
-
+	
+	uint16_t stall_current
+	uint8_t current_position;
+	uint32_t throw_time;
+	uint16_t motor_delay;
+	
 	void motorControl(bool motor_on, bool direction);
+	uint16_t readCurrent();
 
 public:
 	LinearActuator(uint8_t _pin_A, uint8_t _pin_B, uint8_t _pin_enable, uint8_t _pin_current_sense); 
 	~LinearActuator();
+	
+	void setPosition(uint8_t demanded_position); //0 being one end, 255 being the other
+	uint8_t getPosition();
+	void calibrate();
+	void setStallCurrent(uint16_t _stall_current);
+	void setThrowTime(uint32_t _throw_time);
 
+	
 };
 
 #endif // __LA_H_INCLUDED__ 	
